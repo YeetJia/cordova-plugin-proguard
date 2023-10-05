389d69dfdf75e5bc396ba8167148bcfb68e045d3
Tan Yi Jia <tanyj1@greateasternlife.com>
2023-10-05 11:54:09 +0800

    add proguard dependency to root build.gradle

--- scripts/androidBeforeInstall.js
+++ scripts/androidBeforeInstall.js
@@ -19,4 +19,38 @@ module.exports = function (ctx) {
   } catch(err) {
     console.error(err);
   }
+
+  //Root level build.gradle add proguard dependency
+  let rootLevelBuildGradlePath = path.resolve(projectRoot, "./platforms/android/build.gradle");
+  if (fs.existsSync(rootLevelBuildGradlePath)) {
+    let rootBuildGradleContent = fs.readFileSync(rootLevelBuildGradlePath, "utf-8");
+    const splitter = 'classpath';
+    const proguardClassPath = 'com.guardsquare:proguard-gradle:7.1.+';
+
+    if (rootBuildGradleContent.indexOf(splitter) > -1) {
+      if(rootBuildGradleContent.indexOf(proguardClassPath) > -1){
+        console.log(`${POST_CONFIG_TAG} - root build.gradle already added proguard dependency`);
+      } else {
+        let arrContent = rootBuildGradleContent.split(splitter);
+        for(let l = arrContent.length; --l >= 0;){
+          if(arrContent[l] && typeof arrContent[l] === 'string'){
+            let subContent = arrContent[l];
+            let arrSubContent = subContent.split(' ');
+            if(Array.isArray(arrSubContent) && arrSubContent.length > 1){
+              arrSubContent[1]+=`        classpath "${proguardClassPath}"
+`
+            }
+            arrContent[l] = arrSubContent.join(' ');
+          }
+          break;
+        }
+      rootBuildGradleContent = arrContent.join(splitter);
+    } 
+    } else {
+      console.log(`${POST_CONFIG_TAG} - root build.gradle  no ${splitter} found`);
+    }
+    fs.writeFileSync(rootLevelBuildGradlePath, rootBuildGradleContent, "utf-8");
+  } else {
+    console.log(`${POST_CONFIG_TAG} - '${rootLevelBuildGradlePath}' does not exist. root build.gradle proguard dependency config skipped.`);
+  }
 };
